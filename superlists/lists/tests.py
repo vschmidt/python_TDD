1b1986e68ab0df93f4643ced1c0b8bcb390b2637
vschmidt <schmidtvinicio@gmail.com>
2021-09-13 20:07:18 -0300

    Generate template response for POST

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -5,11 +5,12 @@ from lists.views import home_page
 # Create your tests here.
 
 
-class SmokeTest(TestCase):
-    def test_root_url_resolve_to_home_page_view(self):
-        found = resolve("/")
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
+class HomePageTest(TestCase):
+    def test_uses_home_template(self):
         response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertIn("A new list item", response.content.decode())
         self.assertTemplateUsed(response, "home.html")
