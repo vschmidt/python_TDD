d8572fa49e959e7bcb62f254014240ce591b9547
vschmidt <schmidtvinicio@gmail.com>
2021-09-13 13:28:03 -0300

    Change Http to render

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,5 +2,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, "home.html")
