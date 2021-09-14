dbdb7fe86a36a455cd3346a56a416a54a2243d80
vschmidt <schmidtvinicio@gmail.com>
2021-09-14 10:52:41 -0300

    Redirect after POST, and show all items in template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from lists.models import Item
 
-# Create your tests here.
-
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
@@ -10,9 +8,29 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, "home.html")
 
     def test_can_save_a_POST_request(self):
+        self.client.post("/", data={"item_text": "A new list item"})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertIn("A new list item", response.content.decode())
-        self.assertTemplateUsed(response, "home.html")
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["location"], "/")
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = self.client.get("/")
+
+        self.assertIn("itemey 1", response.content.decode())
+        self.assertIn("itemey 2", response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -28,8 +46,7 @@ class ItemModelTest(TestCase):
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
-        first_item_saved = saved_items[0]
-        second_item_saved = saved_items[1]
-
-        self.assertEqual(first_item_saved.text, "The first (ever) list item")
-        self.assertEqual(second_item_saved.text, "Item the second")
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(second_saved_item.text, "Item the second")
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,16 +1,13 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from lists.models import Item
 
 # Create your views here.
 
 
 def home_page(request):
     if request.method == "POST":
-        return render(
-            request,
-            "home.html",
-            {
-                "new_item_text": request.POST.get("item_text", ""),
-            },
-        )
-    return render(request, "home.html")
+        Item.objects.create(text=request.POST["item_text"])
+        return redirect("/")
+
+    items = Item.objects.all()
+    return render(request, "home.html", {"items": items})
