2a7372f9ded9e945316f4c1fe7308a72cc152890
vschmidt <schmidtvinicio@gmail.com>
2021-09-17 21:01:06 -0300

    Add "new" url to lists

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,25 +1,8 @@
-"""superlists URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, re_path
-from lists import views as list_views
+from django.urls import re_path
+from lists import views
 
 urlpatterns = [
-    re_path(r"^$", list_views.home_page, name="home"),
-    re_path(
-        r"^lists/the-only-list-in-the-world/$", list_views.view_list, name="view_list"
-    ),
+    re_path(r"^$", views.home_page, name="home"),
+    re_path(r"^lists/new$", views.new_list, name="new_list"),
+    re_path(r"^lists/the-only-list-in-the-world/$", views.view_list, name="view_list"),
 ]
