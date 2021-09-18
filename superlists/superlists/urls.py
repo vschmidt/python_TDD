77fa70970cd063f477706662d8ef47bc6e42dcd9
vschmidt <schmidtvinicio@gmail.com>
2021-09-17 22:43:55 -0300

    Refactor URLs with include

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,9 +1,8 @@
-from django.urls import re_path
-from lists import views
+from django.urls import include, re_path
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    re_path(r"^$", views.home_page, name="home"),
-    re_path(r"^lists/new$", views.new_list, name="new_list"),
-    re_path(r"^lists/(\d+)/$", views.view_list, name="view_list"),
-    re_path(r"^lists/(\d+)/add_item$", views.add_item, name="add_item"),
+    re_path(r"^$", list_views.home_page, name="home"),
+    re_path(r"^lists/", include(list_urls)),
 ]
